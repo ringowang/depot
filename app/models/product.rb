67ca57af1245fc4faacdbb3c825769ee0fd36959
WangBoguo <wangboguo@gmail.com>
2016-11-01 15:42:36 +0800

    finish task b

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  # 不加allow_blank:true的话，不填会出现两个错误：不能空白和不是jpg结尾，没必要有俩
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
